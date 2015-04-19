6e4402c707fac3af4d53ded97950d13427be4cbc
ad <ad@localhost.localdomain>
2015-04-19 02:11:57 -0300

    alteração de icones da action bar e títulos e mensages

--- gen/br/com/petsoftwaremb/activitys/R.java
+++ gen/br/com/petsoftwaremb/activitys/R.java
@@ -30,11 +30,13 @@ public final class R {
         public static final int background=0x7f020000;
         public static final int ic_action_about=0x7f020001;
         public static final int ic_action_add_person=0x7f020002;
-        public static final int ic_action_help=0x7f020003;
-        public static final int ic_action_search=0x7f020004;
-        public static final int ic_action_settings=0x7f020005;
-        public static final int ic_launcher=0x7f020006;
-        public static final int logo=0x7f020007;
+        public static final int ic_action_event=0x7f020003;
+        public static final int ic_action_help=0x7f020004;
+        public static final int ic_action_new_event=0x7f020005;
+        public static final int ic_action_search=0x7f020006;
+        public static final int ic_action_settings=0x7f020007;
+        public static final int ic_launcher=0x7f020008;
+        public static final int logo=0x7f020009;
     }
     public static final class id {
         public static final int btnTesteNotificacao=0x7f080004;
@@ -64,7 +66,7 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int avisoNotificacao=0x7f050018;
+        public static final int avisoNotificacao=0x7f050019;
         public static final int btn_Cadastrar=0x7f050013;
         public static final int btn_deletar=0x7f050011;
         public static final int btn_listar=0x7f050012;
@@ -72,12 +74,13 @@ public final class R {
         public static final int infoLogin=0x7f05000c;
         public static final int lbCad=0x7f05000f;
         public static final int lbCity=0x7f05000e;
-        public static final int lbFone=0x7f050010;
         /**  CONFIGURAÇÕES  
          */
-        public static final int lbIniciaNTF=0x7f050015;
+        public static final int lbConfiguracoes=0x7f050015;
+        public static final int lbFone=0x7f050010;
+        public static final int lbIniciaNTF=0x7f050016;
         public static final int lbMail=0x7f05000d;
-        public static final int lbRepetirNTF=0x7f050016;
+        public static final int lbRepetirNTF=0x7f050017;
         public static final int login=0x7f05000b;
         public static final int menu_settings=0x7f050002;
         public static final int senha=0x7f050004;
@@ -91,7 +94,7 @@ public final class R {
         public static final int show_label_telefone=0x7f050009;
         /**  NOTIFICAÇÕES  
          */
-        public static final int tituloNotificacao=0x7f050017;
+        public static final int tituloNotificacao=0x7f050018;
         public static final int usuario=0x7f050005;
     }
     public static final class style {
